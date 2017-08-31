8932c0e44d6a63378d3ddba973d7c0678e96dbe0
liaop <2363053754@qq.com>
2017-08-31 09:30:05 +0800

    修改了UI界面

--- app/src/main/java/gos/wxy/ChatActivity.java
+++ app/src/main/java/gos/wxy/ChatActivity.java
@@ -1,38 +1,50 @@
 package gos.wxy;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import gos.wxy.adapter.ChatItemAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+import gos.wxy.adapter.MsgAdapter;
 import gos.wxy.base.ChatItem;
 import gos.wxy.base.EventMsg;
 import gos.wxy.base.Message;
-import gos.wxy.enums.EnumChatType;
 import gos.wxy.enums.EnumEventMode;
 import gos.wxy.tool.Event;
 import gos.wxy.tool.JsonParse;
 
 import static gos.wxy.define.CommandType.COM_CHAT_SEND;
+import static gos.wxy.enums.EnumChatType.OTHER;
+import static gos.wxy.enums.EnumChatType.SELF;
 
-public class ChatActivity extends AppCompatActivity {
-    private final  String TAG = this.getClass().getSimpleName();
+public class ChatActivity extends AppCompatActivity implements OnClickListener{
 
-    Button chatSend;
-    EditText chatInput ;
-    ListView chatListView;
-    ChatItemAdapter chatItemAdapter = new ChatItemAdapter(this);
+    private TextView back;
+    private ImageView imgAccount;
+    private ImageView emoticon;
+    private EditText inputText;
+    private Button sendMessage;
+    private ListView listView;
 
+    private MsgAdapter msgAdapter;
+    private List<ChatItem> msgList = new ArrayList<ChatItem>();
 
     /**
      * 事件接收
@@ -45,7 +57,6 @@ public class ChatActivity extends AppCompatActivity {
                 case COM_CHAT_SEND:     //聊天信息
                     updateChatView(JsonParse.message(msg.getData()));
 
-
                     break;
             }
         }
@@ -54,10 +65,29 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 沉浸式状态栏
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        //requestWindowFeature(Window.FEATURE_NO_TITLE); // 去除标题栏，必须放在R前
         setContentView(R.layout.activity_chat);
+
         Event.register(this);
 
+        initMsg(); // 初始写几条数据，用于测试
         initView();
+    }
+
+    private void initMsg() {
+        ChatItem msg1 = new ChatItem("Hello guy", OTHER);
+        msgList.add(msg1);
+        ChatItem msg2 = new ChatItem("Hello,Who is that?", SELF);
+        msgList.add(msg2);
+        ChatItem msg3 = new ChatItem("this is Sum.", OTHER);
+        msgList.add(msg3);
     }
 
     @Override
@@ -67,46 +97,57 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     private void initView(){
-        chatInput = (EditText)findViewById(R.id.chatInput);
-        chatSend = (Button)findViewById(R.id.chatSend);
-        chatListView = (ListView)findViewById(R.id.chatListView);
-        chatListView.setAdapter(chatItemAdapter);
+        back = (TextView) findViewById(R.id.back);
+        imgAccount = (ImageView) findViewById(R.id.imgAccount);
+        emoticon = (ImageView) findViewById(R.id.emoticon);
+        inputText = (EditText) findViewById(R.id.inputText);
+        sendMessage = (Button) findViewById(R.id.sendMessage);
+        listView = (ListView) findViewById(R.id.chatDialog);
 
-        chatSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String sendMsg = chatInput.getText().toString();
-                chatInput.setText("");
+        back.setOnClickListener(this);
+        imgAccount.setOnClickListener(this);
+        // EditText获得焦点，同时弹出小键盘
+        inputText.requestFocus();
+        emoticon.setOnClickListener(this);
+        sendMessage.setOnClickListener(this);
 
-                Message message = new Message(sendMsg);
-                sendMessage(message);
+        msgAdapter = new MsgAdapter(this, R.layout.chatting_item, msgList);
+        listView.setAdapter(msgAdapter);
 
-                //chatItemAdapter.addChatItem(new ChatItem(sendMsg, EnumChatType.SELF));
-            }
-        });
+    }
 
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.imgAccount:
+                Intent intent = getIntent();
+                String name = intent.getStringExtra("NAME");
+                Toast.makeText(this, "我是" + name, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.emoticon:
 
-        chatInput.addTextChangedListener(new TextWatcher() {//当输入长度大于0时，按钮使能
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
+                break;
+            case R.id.sendMessage:
+                String content = inputText.getText().toString();
+                if(! "".equals(content)) {
+                    Message message = new Message(content);
+                    sendMessage(message);//发送聊天信息给服务器
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
+                    ChatItem chatItem = new ChatItem(message.getMessage(),SELF);
+                    msgList.add(chatItem);
 
-            @Override
-            public void afterTextChanged(Editable s) {
-                int inputLen = s.length();
-
-                if(inputLen >0){
-                    chatSend.setEnabled(true);
-                }else {
-                    chatSend.setEnabled(false);
-
+                    // 当有新消息时，刷新ListView中的显示
+                    msgAdapter.notifyDataSetChanged();
+                    // 将ListView定位到最后一行
+                    listView.setSelection(msgList.size());
+                    // 清空输入框中的内容
+                    inputText.setText("");
                 }
-            }
-        });
+                break;
+        }
     }
 
 
@@ -115,8 +156,13 @@ public class ChatActivity extends AppCompatActivity {
      * @param message
      */
     private void updateChatView(Message message){
-        chatItemAdapter.addChatItem(new ChatItem(message.getMessage(), EnumChatType.SELF));
+        ChatItem chatItem = new ChatItem(message.getMessage(),OTHER);
+        msgList.add(chatItem);
 
+        // 当有新消息时，刷新ListView中的显示
+        msgAdapter.notifyDataSetChanged();
+        // 将ListView定位到最后一行
+        listView.setSelection(msgList.size());
     }
 
     /**
